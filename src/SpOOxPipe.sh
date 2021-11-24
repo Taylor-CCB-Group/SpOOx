f57ace2ca175693aa6fcf6dffb0b0d5b7dc0ab7f
bioinfbloke <stephen.taylor@imm.ox.ac.uk>
2021-11-24 14:41:52 +0000

    updated to latest shell script

--- src/SpOOxPipe.sh
+++ src/SpOOxPipe.sh
@@ -1,26 +1,31 @@
 module load python-cbrg
 module load imctools/2.1.0
+module load zegami-cli/1.5.1 
 
 test=$1
+
 echo Pipeline start
 
 # set up
 incomingDir=/stopgap/hyperion/lho/incoming/
 projectName=covid2
-baseDir=/t1-data/project/covidhyperion/staylor/covid2/
+baseDir=/project/covidhyperion/shared/data/panel2/
 treeDir=$baseDir"tree"
 histocatDir=$baseDir"/histocat/"
 omeDir=$baseDir"/ometiff/"
 confDir=$baseDir"/config/"
 zegamiDir=$baseDir"/zegami/"
+mcdDir=$baseDir"/mcd/"
 zegamiStackDir=$baseDir"/zegamistacks/"
+#markerFile=$confDir"/markers.tsv"
+markerFile=$confDir"/markers_panel2.tsv"
 
 #names of dirs for subdirectries in the tree
 deepCellDir="deepcell/"
 signalExtractionDir="signalextraction/"
-spatialStatsDir="spatialstats"
+spatialStatsDir="spatialstats/"
 
-scriptsDir=/home/s/staylor/hyperion/scripts
+scriptsDir=/home/s/staylor/hyperion/SpOOx/src
 
 # the names of the markers to show in the stacked view in a file
 markersToShow=$baseDir"/config/markers_zegami_stacks.txt"
@@ -34,7 +39,7 @@ function CreateDirs() {
 function MCDToTiff() {
 	#input
 	# find all the directories in incoming and make tiffs
-	find $incomingDir -maxdepth 1 -type d -exec imctools mcdfolder-to-imcfolder {} $omeDir \;
+	find $mcdDir -maxdepth 1 -type d -exec imctools mcdfolder-to-imcfolder {} $omeDir \;
 }
 
 function Tiff2Histocat() {
@@ -55,6 +60,12 @@ function Renamer() {
 	python $scriptsDir"/histocatdirrenamer.py" --indir $histocatDir --pattern Tonsilcontrol --disease_sample TONSIL_SAMPLE_1
 }
 
+function MakeConfig(){
+	# make the template that *MUST* be edited when the QC has been done and the markers that are 
+	# successfull are known
+	echo MakeConfig
+	$test $scriptsDir/"writeConfig.py" --indir $histocatDir --outfile $markerFile
+}
 
 function MakeResultsTree() {
 	# makes the output results tree and populates ROIs with histocat folder
@@ -71,13 +82,14 @@ function DeepCell() {
 	echo DeepCell
 	# need to go into the folder to get the basename
 	#find $treeDir -name "histocat" -exec mkdir {}/../$deepCellDir \; 
-	find $treeDir -name "histocat" -exec $test $scriptsDir/"deepercell.py" --cyt $confDir/cyto.txt --nuc $confDir/nuc.txt --outdir {}/../deepcell/ --dir {} \; 
+	#find $treeDir -name "histocat" -exec $test $scriptsDir/"deepercell.py" --cyt $confDir/cyto.txt --nuc $confDir/nuc.txt --outdir {}/../deepcell/ --dir {} \; 
+	find $treeDir -name "histocat" -exec $test $scriptsDir/"deepercell.py" --markerfile $markerFile --outdir {}/../deepcell/ --indir {} \; 
 }
 
 
 function ZegamiROI() {
-	# generate all the images
-	python $scriptsDir/roi2zegami.py --indir $histocatDir --outdir $zegamiDir --yaml $zegamiDir/zegami.yaml --name $projectName
+	# generate all the images using the histocat directory so this must be up to date
+	$test $scriptsDir/roi2zegami.py --indir $histocatDir --outdir $zegamiDir --yaml $zegamiDir/zegami.yaml --name $projectName
 	# upload to cloud
 	module load zegami-cli/
 	zeg create collections --project XY6PsWre --config $zegamiDir/zegami.yaml
@@ -86,9 +98,14 @@ function ZegamiROI() {
 
 function ZegamiROIStacked() {
 	# zegami stacks
-	python $scriptsDir/flat2stacks.py -i $zegamiDir/img/ -o $zegamiStackDir -y $zegamiStackDir/"zegami.yaml" -n $projectName -d $projectName -m $markersToShow -t $zegamiStackDir/"zegami.tsv"
+	echo ZegamiROIStacked
+	# copy any of the deepcell images to the zegami stack dir
+	find $treeDir -name "deepcell" -exec $test $scriptsDir/"deepercellcopyimages.py" --zegamidir $zegamiStackDir --indir {} \; 
+	$test $scriptsDir/flat2stacks.py -i $zegamiDir/img/ -o $zegamiStackDir -y $zegamiStackDir/"zegami.yaml" -n $projectName -d $projectName -m $markerFile -t $zegamiStackDir/"zegami.tsv"
 	zeg create collections --project XY6PsWre --config $zegamiStackDir/zegami.yaml
 }
+
+
 
 function SignalExtraction() {
 	echo SignalExtraction
@@ -98,7 +115,10 @@ function SignalExtraction() {
 	# not make sure add / on directory names in the cli 
 	# todo use os.path.join rather than +
 	# this runs Signal extraction initially at the ROI level
-	find $treeDir -name "deepcell" -type d -exec $test $scriptsDir/"signalextraction.py" {}/deepcell.tif {}/../histocat/ MAKE_NEW {}/../$signalExtractionDir/ --verbose \;
+	# signalextraction_1
+	#find $treeDir -name "deepcell" -type d -exec $test $scriptsDir/"signalextraction.py" {}/deepcell.tif {}/../histocat/ MAKE_NEW {}/../$signalExtractionDir/ --verbose --distribution_crop_percentile 1.0 \; 
+	# signalextracttion
+	find $treeDir -name "deepcell" -type d -exec $test $scriptsDir/"signalextraction.py" {}/deepcell.tif {}/../histocat/ MAKE_NEW {}/../$signalExtractionDir/ --verbose --distribution_crop_percentile 1.0 --doArcsinh \;
 }
 
 function ZegamiUploadCellImages() {
@@ -109,25 +129,29 @@ function ZegamiUploadCellImages() {
 
 function MergeSignalExtractionAtDifferentScales() {
 	echo MergeSignalExtractionAtDifferentScales
-	# uses signalextraction at the ROI level
-	# this runs SignalExtraction at the SAMPLE level 
-	#find $treeDir -name "SAMPLE*" -exec $test $scriptsDir/"mergecelldata2.py"  --indir {} \;
-	# run at the condition level, there is no pattern to match so need to do it like this
-	find $treeDir -maxdepth 1 -type d -exec $test $scriptsDir/"mergecelldata2.py"  --indir {} \;
-	# on condition level
-	#find $treeDir -maxdepth 2 -exec $test $scriptsDir/"mergecelldata2.py"  --indir {} \;
-	# ./mergecelldata2.py  --indir /t1-data/project/covidhyperion/staylor/covid2/tree/COVID/SAMPLE_10
+	# this merges the signalextraction directories at the SAMPLE level 
+	find $treeDir -name "SAMPLE*" -exec $test $scriptsDir/"mergecelldata.py"  --indir {} \;
+	# run at the condition level, there is no pattern to match so need to do it for each condition
+	find $treeDir -name COVID -type d -exec $test $scriptsDir/"mergecelldata.py"  --indir {} \;
+	find $treeDir -name HEALTHY -type d -exec $test $scriptsDir/"mergecelldata.py"  --indir {} \;
+	find $treeDir -name TONSIL -type d -exec $test $scriptsDir/"mergecelldata.py"  --indir {} \;
 }
 
 function Phenograph() {
 	echo Phenograph
 	# this runs Phenograph at the SAMPLE level since uses maxdepth 3, this needs to be run after 
-	find $treeDir -name "signalextraction" -exec $test $scriptsDir/"phenographclustering.py" -i {}/cellData.tab -o clustering -m /t1-data/project/covidhyperion/staylor/covid2/config/markers_full.tsv clustering -a umap -k 30 -p 3d \;
+	#find $treeDir -name "signalextraction" -exec $test $scriptsDir/"phenographclustering.py" -i {}/cellData.tab -o clustering -m /t1-data/project/covidhyperion/staylor/covid2/config/markers_full.tsv clustering -a umap -k 30 -p 3d \;
+	find $treeDir -name "signalextraction" -exec $test "$scriptsDir/"phenographclustering.py -i {}/cellData.tab -o clustering -m $markerFile clustering -a umap -k 30 -p 3d  --distribution_crop_percentile 0.99 --normalise_intensities \;
+
 }
 
 function SpatialStatistics() {
 	echo SpatialStatistics 
-	find $treeDir -name "clustering" -exec $test $scriptsDir/"spatialstats.py" -i {}/cellData.tab -c $confDir/exampleclusterannotation.tab -f celllocationmap paircorrelationfunction --output {}../$spatialStatsDir
+	#$scriptsDir/"spatialstats.py" -i /t1-data/project/covidhyperion/shared/data/panel2/tree/COVID/clustering/cellData.tab -c /t1-data/project/covidhyperion/shared/data/panel2/tree/COVID/clustering/annotations.tab --output /t1-data/project/covidhyperion/shared/data/panel2/tree/COVID/spatialstats/ 
+	$scriptsDir/"spatialstats.py" -i /t1-data/project/covidhyperion/shared/data/panel2/tree/COVID/clustering/cellData.tab -c /t1-data/project/covidhyperion/shared/data/panel2/tree/COVID/clustering/annotations.tab -f paircorrelationfunction --output /t1-data/project/covidhyperion/shared/data/panel2/tree/COVID/spatialstats/ 
+
+
+	#find $treeDir -name "clustering" -exec $test $scriptsDir/"spatialstats.py" -i {}/cellData.tab -c /t1-data/project/covidhyperion/shared/data/panel2/tree/COVID/clustering/annotations.tab --output {}../$spatialStatsDir \;
 }
 
 
@@ -136,6 +160,7 @@ function SpatialStatistics() {
 #Tiff2Histocat
 #Renamer
 #ZegamiROI
+#MakeConfig
 #MakeResultsTree
 #DeepCell
 #ZegamiROIStacked
@@ -143,7 +168,8 @@ function SpatialStatistics() {
 #MergeSignalExtractionAtDifferentScales
 #ZegamiUploadCellImages
 #Phenograph
+#SpatialStatistics
 #TODO
 #Harmony
-#SpatialStatistics
+
 
