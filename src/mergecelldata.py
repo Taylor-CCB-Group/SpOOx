983618c0e8835a98d69f6184e6b626918a8f1900
Simon McG <simon.mcgowan@imm.ox.ac.uk>
2022-05-06 15:58:58 +0100

    Update mergecelldata.py
    edited to work with expected file structure and to add min & max filters for 'area' in the merged data

--- src/mergecelldata.py
+++ src/mergecelldata.py
@@ -7,44 +7,30 @@ import os.path
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-def FindAll(name, path):
+def FindAll(name, indir):
     # name of file
     # starting path
-    # only look dir
     result = []
-    for root, dirs, files in os.walk(path):
-        if name in dirs:
+    for root, dirs, files in os.walk(indir):
+        if name in files:
             path = os.path.join(root, name)
-            # only use the ROI data otherwise will start concantenating all the subdirs
             if "ROI" in path:
                 result.append(os.path.join(root, name))
     return result
 
-def GetSampleDir(cellDataFile):
-    print("cellDataFile = ", cellDataFile)
-    dirname = os.path.dirname
-    return(dirname(dirname(dirname(cellDataFile))))
 
-def GetSourceDir(cellDataFile):
-    print("cellDataFile = ", cellDataFile)
-    dirname = os.path.dirname
-    return(dirname(dirname(dirname(dirname(cellDataFile)))))
-
-def GetHeader(file):
-    with open(file) as f:
-        return(f.readline())
-
-
-def AppendFiles(dirList, inputFile, outputFile):
-    # take each directory and append the file celldata file in there into a dataframe
-    df = pd.concat((pd.read_csv(os.path.join(dir,inputFile),sep="\t") for dir in dirList))
+def AppendFiles(fileList, outputFile, minAreaVal, maxAreaVal):
+    # take each directory and append the celldata file therein to a dataframe
+    df = pd.concat((pd.read_csv(file,sep="\t") for file in fileList))
+    # filter using values in 'area' column of dataframe
+    df = df[(df.area >= minAreaVal) & (df.area <= maxAreaVal)]
     outputDir = os.path.dirname(outputFile)
     beforeClean = os.path.join(outputDir,"beforecleaning.tab")
-    pandasReport = os.path.join(outputDir,"pandasprofile.html")
+    df.to_csv(beforeClean,sep="\t", index=False)
     # gets rid of any columns that don't line up with the header
     afterClean = df.dropna(axis='columns')
     afterClean.to_csv(outputFile,sep="\t", index=False)
-    df.to_csv(beforeClean,sep="\t", index=False)
+    pandasReport = os.path.join(outputDir,"pandasprofile.html")
     profile = ProfileReport(df, title="Pandas Profiling Report", minimal=True, progress_bar=False)
     profile.to_file(pandasReport)
 
@@ -57,10 +43,12 @@ parser.add_argument('--excludeList', dest='excludeList', nargs='+',
                     help='list of dirs to exclude as a pattern if you have bad samples for example e.g. --excludeList SAMPLE_11/ROI_1/ SAMPLE_10/ SAMPLE_17/')
 parser.add_argument('--infile', dest='infile', default = "cellData.tab",
                     help='the file it will look to merge')		
-parser.add_argument('--outfile', dest='outfile', default = "cellData.tab",
+parser.add_argument('--outfile', dest='outfile', default = "mergecellData.tab",
                     help='the file to write out')		
-parser.add_argument('--directory', dest='directory', default = "signalextraction",
-                    help='the name of the directory to write to')		
+parser.add_argument('--minarea', dest='minarea', default = "5", nargs='?', const=1, type=int,
+                    help='minimum value for area of each cell')		
+parser.add_argument('--maxarea', dest='maxarea', default = "300", nargs='?', const=1, type=int,
+                    help='maximum value for area of each cell')		
 
 if len(sys.argv)==1:
     parser.print_help(sys.stderr)
@@ -68,23 +56,22 @@ if len(sys.argv)==1:
 
 args = parser.parse_args()
 
-#  typical path 
-# /t1-data/project/covidhyperion/staylor/covid2/tree/TONSIL/SAMPLE_1/ROI_2/signalextraction/cellData.tab
-#indir = "/t1-data/project/covidhyperion/staylor/covid2/tree/COVID/SAMPLE_16"
-#directory = "signalextraction"
+#  examples:
+#indir = "/full/path/to/project/signalextraction/"
 #infile = "cellData.tab"
-#outfile = "cellData.tab"
 #excludeList = ["SAMPLE_11/", "SAMPLE_10/", "SAMPLE_17/"]
 #includeList="SAMPLE_1/  SAMPLE_10/  SAMPLE_11/  SAMPLE_16/  SAMPLE_17/"
-#excludeList =[]
+
 
 infile=args.infile
-directory = args.directory
 indir=args.indir
+name=args.infile
 outfile=args.outfile
 excludeList = args.excludeList
+minArea = args.minarea
+maxArea = args.maxarea
 
-allCellDataFiles = FindAll(directory, indir)
+allCellDataFiles = FindAll(name, indir)
 print("Initial data files to process",allCellDataFiles)
 
 if (args.excludeList):
@@ -92,13 +79,8 @@ if (args.excludeList):
         if not any(word in sent for word in excludeList)]
     print("Remaining files after exclusion:",allCellDataFiles)
 
+outfile = os.path.join(indir,outfile)
+print("Writing files from:\n","\n",allCellDataFiles,"\n*** to ***\n",outfile)
 
-newDir = os.path.join(indir, directory)
-os.makedirs(newDir, exist_ok=True)
-outfile = os.path.join(newDir,outfile)
-
-print("Writing files from:\n","\n",allCellDataFiles,"\n*** to ***\n",os.path.join(indir,outfile))
-
-#ConcatenateFiles(allCellDataFiles, infile, os.path.join(indir,outfile))
-AppendFiles(allCellDataFiles, infile, os.path.join(indir,outfile))
+AppendFiles(allCellDataFiles, outfile, minArea, maxArea)
 
